a63c879651b99da6ab5d9f29282d11232be0747a
PanRagon <thebigecko@gmail.com>
2019-06-14 20:27:46 +0200

    Create insertion sort

--- Sorting Algorithms/sort.js
+++ Sorting Algorithms/sort.js
@@ -1,6 +1,6 @@
 var unsortedArray = [];
 
-while(unsortedArray.length < 100) {
+while(unsortedArray.length < 10) {
 	unsortedArray.push(Math.floor(Math.random() * 1001));
 }
 
@@ -26,8 +26,22 @@ function bubbleSort(arr) {
 	}
 
 	while(swap);
+	console.log(arr);
 	return arr;
 }
 
-let sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray);
+//Insertion Sort
+
+function insertionSort(arr) {
+	for(var i = 0; i < arr.length; i++) {
+		let store = arr[i];
+		for(var j = i - 1; j > -1 && arr[j] > store; j--) {
+			arr[j + 1] = arr[j]
+		}
+		arr[j + 1] = store;
+	}
+	return arr;
+}
+
+let sortedArray = insertionSort(unsortedArray);
+console.log(sortedArray)
